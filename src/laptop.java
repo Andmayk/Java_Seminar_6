1a333d5c4ccd3de299156813916172a370be74f1
AndMay <am@42.ru>
2023-10-12 22:48:29 +0700

    begin homework

---
+++ src/laptop.java
@@ -0,0 +1,48 @@
+public class laptop {
+    private String manufacturer;
+    private int ram;
+    private int hdd;
+    private String os;
+    private String color;
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
